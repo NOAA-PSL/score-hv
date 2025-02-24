4c6a56c3565d2cb2bc036edcafa54aa376619fb9
Sherrie Fredrick <Sherrie.Fredrick@noaa.gov>
2025-02-24 10:30:56 -0700

    Added the harvester for reading SOCA JEDI files.

--- src/score_hv/hv_registry.py
+++ src/score_hv/hv_registry.py
@@ -10,6 +10,7 @@ from score_hv.harvesters.innov_netcdf import InnovStatsCfg, InnovStatsHv
 from score_hv.harvesters.obs_log import ObsInfoCfg, ObsInfoHv
 from score_hv.harvesters.inc_logs import LogIncCfg, LogIncHv
 from score_hv.harvesters.daily_bfg import DailyBFGConfig, DailyBFGHv
+from score_hv.harvesters.soca_diags import SOCADiagsConfig, SOCADiagsHv
 from score_hv.harvesters.gsi_satellite_radiance_channel import GSISatelliteRadianceChannelConfig, GSISatelliteRadianceChannelHv
 
 NAMED_TUPLES_LIST = 'tuples_list'
@@ -18,6 +19,8 @@ INNOV_NETCDF = 'innov_stats_netcdf'
 OBS_INFO_LOG = 'obs_info_log'
 INC_LOGS = 'inc_logs'
 DAILY_BFG = 'daily_bfg'
+SOCA_DIAGS = 'soca_diags'
+
 GSI_SATELLITE_RADIANCE_CHANNEL = 'gsi_satellite_radiance_channel'
 
 Harvester = namedtuple('Harvester', ('name', 'config_handler', 'data_parser'),)
@@ -42,9 +45,14 @@ harvester_registry = {INNOV_NETCDF: Harvester(
                           LogIncHv
                           ),
                       DAILY_BFG: Harvester(
-                          'Daily mean statistics from background forecast data',
+                          'Daily mean statistics from background forecast',
                           DailyBFGConfig,
                           DailyBFGHv
+                          ),
+                      SOCA_DIAGS: Harvester(
+                          'Statistics from SOCA diagnostics files',
+                          SOCADiagsConfig,
+                          SOCADiagsHv
                           ),
                       GSI_SATELLITE_RADIANCE_CHANNEL: Harvester(
                           'Satellite radiance statistics by channel from the '
