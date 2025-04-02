a7345dd88611a30328685dbc2d0a790621d4de00
Sherrie Fredrick <Sherrie.Fredrick@noaa.gov>
2025-04-02 10:52:04 -0600

    changed all of the if len(harvested_data) != len(groups_wanted): statements to assert statements.  Changed all variables to variables.  Changed all statistic to statistics

--- tests/test_insitu_profile.py
+++ tests/test_insitu_profile.py
@@ -30,8 +30,8 @@ SOCA_PATH = [os.path.join(TEST_DATA_PATH,
 
 VALID_CONFIG_DICT = {'harvester_name': hv_registry.SOCA_DIAGS,
                      'filenames' : SOCA_PATH,
-                     'statistic': ['mean', 'median', 'StdDev',  'minimum', 'maximum'],
-                     'variable': ['salinity','waterTemperature'],
+                     'statistics': ['mean', 'median', 'StdDev',  'minimum', 'maximum'],
+                     'variables': ['salinity','waterTemperature'],
                      }
 
 def test_soca_harvester():
@@ -44,7 +44,7 @@ def test_verify_filename_components():
     expected_variable_names = ['salinity','waterTemperature']
     data1 = harvest(VALID_CONFIG_DICT)
     for item in data1:
-        if item.variable not in expected_variable_names:
+        if item.variables not in expected_variable_names:
            raise ValueError(f"Error: {item.variable} is not in the expected variable list.") 
         assert item.sensor == None   
         assert item.satellite == 'argo'
@@ -70,13 +70,13 @@ def test_verify_group_mean_values(tolerance=0.001):
     waterTemperature_means = [7.605209,0.053602,0.049655]
     groups_wanted = ['ObsValue','oman','ombg']
     for item in data1:
-        if item.variable == 'salinity' and item.statistic == 'mean':
+        if item.variables == 'salinity' and item.statistics == 'mean':
            for i, group_name in enumerate(groups_wanted):
                if item.group == group_name:
                   calc_value = salinity_means[i]
                   harvested_value = item.value
                   assert abs(harvested_value - calc_value) <= tolerance 
-        elif item.variable == 'waterTemperature' and item.statistic == 'mean':    
+        elif item.variables == 'waterTemperature' and item.statistics == 'mean':    
              for i, group_name in enumerate(groups_wanted):
                  if item.group == group_name:
                     calc_value = waterTemperature_means[i]
@@ -89,13 +89,13 @@ def test_verify_group_median_values(tolerance=0.001):
     waterTemperature_medians = [5.421991,0.00878,0.008301]
     groups_wanted = ['ObsValue','oman','ombg']
     for item in data1:
-        if item.variable == 'salinity' and item.statistic == 'median':
+        if item.variables == 'salinity' and item.statistics == 'median':
            for i, group_name in enumerate(groups_wanted):
                if item.group == group_name:
                   calc_value = salinity_medians[i]
                   harvested_value = item.value
                   assert abs(harvested_value - calc_value) <= tolerance 
-        elif item.variable == 'waterTemperature' and item.statistic == 'median':    
+        elif item.variables == 'waterTemperature' and item.statistics == 'median':    
              for i, group_name in enumerate(groups_wanted):
                  if item.group == group_name:
                     calc_value = waterTemperature_medians[i]
@@ -108,13 +108,13 @@ def test_verify_group_standard_deviation_values(tolerance=0.001):
     waterTemperature_StdDev = [6.19354,0.749902,0.772137]
     groups_wanted = ['ObsValue','oman','ombg']
     for item in data1:
-        if item.variable == 'salinity' and item.statistic == 'StdDev':
+        if item.variables == 'salinity' and item.statistics == 'StdDev':
            for i, group_name in enumerate(groups_wanted):
                if item.group == group_name:
                   calc_value = salinity_StdDev[i]
                   harvested_value = item.value
                   assert abs(harvested_value - calc_value) <= tolerance 
-        elif item.variable == 'waterTemperature' and item.statistic == 'StdDev':    
+        elif item.variables == 'waterTemperature' and item.statistics == 'StdDev':    
              for i, group_name in enumerate(groups_wanted):
                  if item.group == group_name:
                     calc_value = waterTemperature_StdDev[i]
@@ -127,13 +127,13 @@ def test_verify_group_minimum_values(tolerance=0.001):
     waterTemperature_minimums = [-1.015997,-8.81186,-8.81186]
     groups_wanted = ['ObsValue','oman','ombg']
     for item in data1:
-        if item.variable == 'salinity' and item.statistic == 'minimum':
+        if item.variables == 'salinity' and item.statistics == 'minimum':
            for i, group_name in enumerate(groups_wanted):
                if item.group == group_name:
                   calc_value = salinity_minimums[i]
                   harvested_value = item.value
                   assert abs(harvested_value - calc_value) <= tolerance 
-        elif item.variable == 'waterTemperature' and item.statistic == 'minimum':    
+        elif item.variables == 'waterTemperature' and item.statistics == 'minimum':    
              for i, group_name in enumerate(groups_wanted):
                  if item.group == group_name:
                     calc_value = waterTemperature_minimums[i]
@@ -146,13 +146,13 @@ def test_verify_group_maximum_values(tolerance=0.001):
     waterTemperature_maximums = [30.451014,7.235542,7.235542]
     groups_wanted = ['ObsValue','oman','ombg']
     for item in data1:
-        if item.variable == 'salinity' and item.statistic == 'maximum':
+        if item.variables == 'salinity' and item.statistics == 'maximum':
            for i, group_name in enumerate(groups_wanted):
                if item.group == group_name:
                   calc_value = salinity_maximums[i]
                   harvested_value = item.value
                   assert abs(harvested_value - calc_value) <= tolerance 
-        elif item.variable == 'waterTemperature' and item.statistic == 'maximum':    
+        elif item.variables == 'waterTemperature' and item.statistics == 'maximum':    
              for i, group_name in enumerate(groups_wanted):
                  if item.group == group_name:
                     calc_value = waterTemperature_maximums[i]
