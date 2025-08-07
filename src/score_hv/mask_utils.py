b863baadecd9068eb6f5344ced26587b88a31360
Adam Schneider <adam.schneider@noaa.gov>
2025-08-07 16:40:42 -0600

    minor clean up, including removal of old development unit test files and commentetd out code

--- src/score_hv/mask_utils.py
+++ src/score_hv/mask_utils.py
@@ -86,41 +86,6 @@ class MaskCatalog:
             masked_frac = xr.where(variable_data.notnull(), 1.0, np.nan)
         
         return(masked_variable, masked_frac)
-
-    '''
-    def replace_bad_values_with_nan(self,variable_data):
-        """
-          Check for _FillValue or missing_values in the variable data.
-          This method will replace the missing or fill values with 
-          NaN.
-          Parameters;
-          variable_data - The variable field data. Variable_data is of
-                          type class 'xarray.core.dataarray.DataArray.
-          Return - The variable field data with NaN's for where the 
-                   grid values are missing or fill values. The returned
-                   masked_variable_data is of type 
-                   class 'xarray.core.dataarray.DataArray.
-          """         
-        fill_value = variable_data.encoding.get('_FillValue', None)
-        missing_value = variable_data.encoding.get('missing_value', None)
-     
-        """
-          Create a combined mask for fill_values,missing_value and non
-          finite values. 
-          """
-        if fill_value is not None and missing_value is not None:
-           mask = (variable_data != fill_value) & (variable_data != missing_value)
-        elif fill_value is not None:
-           mask = variable_data != fill_value
-        elif missing_value is not None:
-           mask = variable_data != missing_value
-        else:
-           mask = np.isfinite(variable_data)
-
-       # Apply the mask
-        masked_variable = variable_data.where(mask,np.nan)
-        return(masked_variable)
-    '''
     
     def user_mask(self, mask_type, variable_data, fraction_data, lfrac,
                   sotyp_data, icec=None):
