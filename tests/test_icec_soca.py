316402be3262ac7459e12fb150d824101b22c6e7
Sherrie Fredrick <Sherrie.Fredrick@noaa.gov>
2026-06-04 09:01:14 -0600

    changed the config_dict to     config_dict = {         'harvester_name': hv_registry.SOCA_DIAGS,         'filenames': [str(file_path)],         'statistics': ['mean', 'median', 'StdDev', 'minimum', 'maximum', 'rmse'],         'variables': [meta['variable']],         'QC_threshold': meta['QC_threshold'],

--- tests/test_icec_soca.py
+++ tests/test_icec_soca.py
@@ -12,7 +12,8 @@ DATA_DIR = BASE_DIR / 'data'
 YAML_PATH = BASE_DIR / "icec_nsidc_nh.yaml"
 
 with open(YAML_PATH, 'r') as f:
-    TEST_REGISTRY = yaml.safe_load(f)
+    raw_data = yaml.safe_load(f) 
+    TEST_REGISTRY = raw_data.get('TEST_REGISTRY', raw_data)
 
 def verify_filename_components(data_record, expected_components, expected_variable):
     """
@@ -93,21 +94,11 @@ def test_soca_harvester(test_id):
         'statistics': ['mean', 'median', 'StdDev', 'minimum', 'maximum', 'rmse'],
         'variables': [meta['variable']],
         'QC_threshold': meta['QC_threshold'],
-        'ocean_depth_bins': meta.get('ocean_depth_bins') or comp.get('ocean_depth_bins'),
-        'file_region': comp.get('file_region', 'global'),
-        'components': {
-            'variables': meta['variable'],  
-            'sensor': comp.get('sensor'),
-            'satellite': comp.get('satellite'),
-            'level': comp.get('level'),
-        }        
     }
 
-    # 1. Execute Harvester
     all_data = harvest(config_dict)
     assert len(all_data) > 0, "No data was harvested."
     
-    # 2. Verification (Using external helpers)
     verify_filename_components(all_data[0], comp, meta['variable'])
     verify_statistics(all_data, meta['expected_stats'], test_id)
 
